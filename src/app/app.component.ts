8b94298621bd254273bdf1a54ef87ee6302778b1
Leonardo Medina <leo_medina14@hotmail.com>
2021-08-25 00:20:11 -0500

    Google Analytics y Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { Product } from './interfaces/Product';
+import { NavigationEnd, Router } from '@angular/router'
+import { filter } from 'rxjs/operators'
+
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -8,10 +11,13 @@ import { Product } from './interfaces/Product';
 })
 export class AppComponent {
 
-  
+  constructor(private router: Router) {
+    const navEndEvent$ = this.router.events.pipe(filter( event => event instanceof NavigationEnd));
 
-  
-  
+    navEndEvent$.subscribe((event: NavigationEnd) => {
+      gtag('config', 'G-N72GF8YW31', {page_path: event.urlAfterRedirects});
+    });
 
-  
+  }
+
 }
